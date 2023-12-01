f712184fde3b11d4c5ec25ea89166778fd62544f
ArthurLashermes <alashermes@openstudio.fr>
2023-12-01 15:28:32 +0100

    add translation

---
+++ I18n/backOffice/default/fr_FR.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+    'Action' => 'Action',
+    'Availability in stock' => 'Disponibilité en stock',
+    'Color attribut id' => 'Les ids des attributs lié à une couleur (bleu,vert,rouge ..)',
+    'Date' => 'Date',
+    'Delete' => 'Supprimer',
+    'Download' => 'Télécharger',
+    'Export facebook feed' => 'Exporter le flux Facebook',
+    'Facebook Feed' => 'Flux Facebook',
+    'Facebook Feed Configuration' => 'Configuration du flux Facebook',
+    'File name' => 'Nom du fichier',
+    'If the box is checked then will only export products with stock' => 'Si la case est cochée, seulement les produits avec du stock seront exportés.',
+    'Old feed' => 'Flux anciennement exporté(s)',
+    'Please note the export may take several minutes to complete.' => 'Attention l\'export peut prendre plusieurs minutes à finir en fonction du nombre de produit dans votre boutique.',
+    'Save' => 'Sauvegarder',
+    'Size attribut id' => 'Les ids des attributs lié à une taille (XL,L,M,S ...)',
+    'This field corresponds to the list of attribute ids intended to define the color(the form is 14,12,1,4).' => 'Ce champ correspond à la liste des identifiants d\'attributs destinés à définir la couleur (la forme est 14,12,1,4).',
+    'This field corresponds to the list of attribute ids intended to define the size (the form is 14,12,1,4).' => 'Ce champ correspond à la liste des identifiants d\'attributs destinés à définir la taille (la forme est 14,12,1,4).',
+);
