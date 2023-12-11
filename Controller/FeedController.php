5cdfa49600a1f2b7623525ae733312ad8a8a150d
ArthurLashermes <alashermes@openstudio.fr>
2023-12-11 16:57:38 +0100

    better security

--- Controller/FeedController.php
+++ Controller/FeedController.php
@@ -5,31 +5,21 @@ namespace FacebookFeed\Controller;
 use FacebookFeed\FacebookFeed;
 use Symfony\Component\Routing\Annotation\Route;
 use Thelia\Controller\Front\BaseFrontController;
-use Thelia\Core\HttpFoundation\Request;
 use Thelia\Core\HttpFoundation\Response;
 use Thelia\Tools\URL;
 
 class FeedController extends BaseFrontController
 {
-    #[Route('/facebookfeed/{filename}/feed.csv', name: 'FacebookFeed_csv', methods: 'GET')]
-    public function getCSVFeed(Request $request,$filename)
+    #[Route('/facebookfeed/feed', name: 'FacebookFeed_csv', methods: 'GET')]
+    public function getCSVFeed()
     {
-        $fileExtension = pathinfo($filename, PATHINFO_EXTENSION);
-
-        if (!in_array($fileExtension, ['csv'])) {
+        $fileName = FacebookFeed::EXPORT_DIR.DS.'fluxfacebook.csv';
+        if (!file_exists($fileName)){
             return $this->generateRedirect(
                 URL::getInstance()->absoluteUrl('/')
             );
         }
-
-        $dir = dirname(realpath(FacebookFeed::EXPORT_DIR.'/'.$filename));
-        if ($dir !== FacebookFeed::EXPORT_DIR){
-            return $this->generateRedirect(
-                URL::getInstance()->absoluteUrl('/')
-            );
-        }
-
-        $content = file_get_contents(FacebookFeed::EXPORT_DIR.'/'.$filename);
+        $content = file_get_contents($fileName);
 
         $response = new Response();
         $response->setContent($content);
